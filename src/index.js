b0e085b04a80872c55bab22272478137e9da8035
abadan <abadankaristad@gmail.com>
2025-04-15 06:29:52 -0300

    ajustes

--- src/index.js
+++ src/index.js
@@ -34,6 +34,6 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BlocoNotas />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
