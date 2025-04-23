ae719246c8b10d3833880c79ef2fe2d0546b16fc
abadan <abadankaristad@gmail.com>
2025-04-23 18:21:57 -0300

    alt

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://backend-sa9a.onrender.com'
+  // baseURL: 'https://backend-sa9a.onrender.com'
+  baseURL: 'http://localhost:3333/'
 
 });
 
