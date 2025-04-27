ad08f754072856855fdfa0c7fe15563c1d9efb87
abadan <abadankaristad@gmail.com>
2025-04-27 08:27:26 -0300

    alt notas

--- src/pages/blocoNotas/blocoNotas.js
+++ src/pages/blocoNotas/blocoNotas.js
@@ -3,15 +3,13 @@ import React,  { useState, useEffect} from 'react';
 import api from '../../services/api.js';
 
 import styles from './blocoNotas.module.css';
-import './styles/sidebar.css';
 
+import CadernoNotas from './components/cadernoNotas.js'; 
 import Notes from './components/notas.js'
 
 
 const BlocoNotas = () => {
   
-  const [title, setTitles] = useState((''));
-  const [notes, setNotes] = useState((''));
   const [allNotes, setAllNotes] = useState([]);
 
   useEffect(() => {
@@ -25,89 +23,19 @@ const BlocoNotas = () => {
   
   async function handleSubmit(e) {
     e.preventDefault();
-    
     const response = await api.post('/annotations', {
-      title,
-      notes,
+      title: e.target[0].value,
+      notes:  e.target[1].value,
       priority: false
     });
-
-    setTitles('');
-    setNotes('');
 
     setAllNotes([...allNotes, response.data]);
   }
 
-  useEffect(() => {
-    async function enableSubmitButton() {
-      let btn = document.getElementById('btn_sumit');
-      btn.style.background = '#FFD3CA';
-      if (title !== '' && notes !== '') {
-        btn.style.background = '#EB8F7A';
-      }
-    }
-
-    enableSubmitButton();
-  }, [title, notes]);
-
-
-
-  //funções em teste----------------------------------------------
-  function handleKeyUp(e) {
-    e.preventDefault();
-    if(e.key === 'Enter'){
-      const txa = document.getElementById('txarea');
-      const locCursor = txa.selectionStart;
-      var val = txa.value;
-      txa.value = val.slice(0, locCursor) + "○ " + val.slice(locCursor);
-      txa.selectionEnd = locCursor + 3;
-    }
-  
-  }
-
-  function handleFocus(e) {
-    setTimeout(()=>{
-      e.preventDefault();
-      if(document.getElementById('txarea').value === ''){
-        document.getElementById('txarea').value ='○ ';
-      }
-    }, 200);
-  }
-  //funções em teste----------------------------------------------
-
-
   return (
     <div className={styles.ModuloBlocoNotas}>
 
-      <aside>
-        <strong>Caderno de Notas</strong>
-        <form onSubmit={handleSubmit}>
-
-          <div className="input-block">
-            <label htmlFor="title">Título da Anotação</label>
-            <input
-              required
-              value={title}
-              onChange={e => setTitles(e.target.value)}
-            />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="nota">Anotações</label>
-            <textarea
-            id="txarea"
-              onKeyUp={handleKeyUp}//em teste
-              onFocus={handleFocus}//em teste
-              required
-              value={notes}
-              onChange={e => setNotes(e.target.value)}
-            />
-          </div>
-
-        <button id="btn_sumit" type="submit">Salvar</button>
-
-        </form>
-      </aside>
+      <CadernoNotas f_handleSubmit={handleSubmit}/>
 
       <main>
         <ul>
---
+++ src/pages/blocoNotas/components/cadernoNotas.js
@@ -0,0 +1,82 @@
+import React,  { useState, useEffect} from 'react';
+
+import styles from './cadernoNotas.module.css';
+
+
+const CadernoNotas = ({f_handleSubmit}) => {
+  
+  const [title, setTitles] = useState((''));
+  const [notes, setNotes] = useState((''));
+
+  useEffect(() => {
+    async function enableSubmitButton() {
+      let btn = document.getElementById('btn_sumit');
+      btn.style.background = '#FFD3CA';
+      if (title !== '' && notes !== '') {
+        btn.style.background = '#EB8F7A';
+      }
+    }
+
+    enableSubmitButton();
+  }, [title, notes]);
+
+
+
+  //funções em teste----------------------------------------------
+  function handleKeyUp(e) {
+    e.preventDefault();
+    if(e.key === 'Enter'){
+      const txa = document.getElementById('txarea');
+      const locCursor = txa.selectionStart;
+      var val = txa.value;
+      txa.value = val.slice(0, locCursor) + "○ " + val.slice(locCursor);
+      txa.selectionEnd = locCursor + 3;
+    }
+  
+  }
+
+  function handleFocus(e) {
+    setTimeout(()=>{
+      e.preventDefault();
+      if(document.getElementById('txarea').value === ''){
+        document.getElementById('txarea').value ='○ ';
+      }
+    }, 200);
+  }
+  //funções em teste----------------------------------------------
+
+
+  return (
+    <aside className={styles.aside}>
+        <strong>Caderno de Notas</strong>
+        <form onSubmit={f_handleSubmit}>
+
+          <div className={styles.inputBlock}>
+            <label htmlFor="title">Título da Anotação</label>
+            <input
+              required
+              value={title}
+              onChange={e => setTitles(e.target.value)}
+            />
+          </div>
+
+          <div className={styles.inputBlock}>
+            <label htmlFor="nota">Anotações</label>
+            <textarea
+            id="txarea"
+              onKeyUp={handleKeyUp}//em teste
+              onFocus={handleFocus}//em teste
+              required
+              value={notes}
+              onChange={e => setNotes(e.target.value)}
+            />
+          </div>
+
+        <button id="btn_sumit" type="submit">Salvar</button>
+
+        </form>
+      </aside>
+  );
+}
+
+export default CadernoNotas;
