180751afe60b7ff6b9fcc7d585a0a34c01658693
abadan <abadankaristad@gmail.com>
2025-05-21 12:34:09 -0300

    incluir radio button

--- src/pages/blocoNotas/blocoNotas.js
+++ src/pages/blocoNotas/blocoNotas.js
@@ -47,11 +47,31 @@ const BlocoNotas = () => {
   return (
     <div className={styles.ModuloBlocoNotas}>
 
+      {/* MENU SUPERIOR DE NOTAS */}
       <div className={styles.nav}>
         <div>
           <button onClick={() => setNewNoteOpen(true)}>Nova Nota</button>
+          <section className={styles.radioGroup}>
+            <label className={styles.radioLabel}>
+              <input type="radio" name="priority" id="all" className={styles.radioInput} defaultChecked />
+              <span className={styles.customRadio}></span>
+              Todas
+            </label>
+            <label className={styles.radioLabel}>
+              <input type="radio" name="priority" id="priority" className={styles.radioInput} />
+              <span className={styles.customRadio}></span>
+              Prioridade
+            </label>
+            <label className={styles.radioLabel}>
+              <input type="radio" name="priority" id="completed" className={styles.radioInput} />
+              <span className={styles.customRadio}></span>
+              Normais
+            </label>
+          </section>
         </div>
       </div>
+
+      {/* FORM PARA INCLUIR NOTAS */}
       <div>
         <NewNote
           isOpen={NewNoteOpen}
@@ -60,6 +80,7 @@ const BlocoNotas = () => {
         />
       </div>
 
+      {/* LISTA DE NOTAS */}
       <main>
         <ul>
           {allNotes.map(data => (
