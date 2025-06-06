5c1b7196ebc1c98b71763595fabd0037e9ac7e83
abadan <abadankaristad@gmail.com>
2025-06-06 18:09:28 -0300

    env

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://backend-sa9a.onrender.com'
+  // baseURL: 'https://backend-sa9a.onrender.com',
   baseURL: 'http://localhost:3333',
   withCredentials: true, // importante para enviar cookies
 });
