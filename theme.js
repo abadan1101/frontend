b592ff3b868c83f83c2435049520219b8400fd7b
abadan <abadankaristad@gmail.com>
2025-06-24 12:28:05 -0300

    Updates

--- theme.js
+++ theme.js
@@ -1,11 +1,4 @@
-function setFaviconByTheme() {
-  const favicon = document.getElementById('favicon');
-  const appleicon = document.getElementById('appleicon');
-  const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  const icon = isDark ? '%PUBLIC_URL%/logo5.png' : '%PUBLIC_URL%/logo4.png';
-  if (favicon) favicon.href = icon;
-  if (appleicon) appleicon.href = icon;
-}
+//MÉTODO PARA CONFIGURAR TEMA DA BARRA DE TAREFAS DO NAVEGADOR
 function setThemeColorByTheme() {
   const themeMeta = document.querySelector('meta[name="theme-color"]');
   const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
@@ -13,9 +6,7 @@ function setThemeColorByTheme() {
     themeMeta.setAttribute('content', isDark ? '#23243a' : '#ffffff');
   }
 }
-setFaviconByTheme();
 setThemeColorByTheme();
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
-  setFaviconByTheme();
   setThemeColorByTheme();
 });
