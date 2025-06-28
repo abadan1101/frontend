c1836c64d7d6058b83cbd48a1faa1880cf7ba554
abadan <abadankaristad@gmail.com>
2025-06-28 07:33:56 -0300

    alt

--- notepad/src/services/api.js
+++ notepad/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://backend-nu-six-69.vercel.app/',
+  baseURL: 'https://backend-nu-six-69.vercel.app/',
   // baseURL: 'https://backend-vxo5.onrender.com',
-  baseURL: 'http://localhost:3333',
+  // baseURL: 'http://localhost:3333',
   withCredentials: true, // importante para enviar cookies
 });
 
